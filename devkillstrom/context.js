cdf6c95c6aec010578dcf80bee69dc66ee042454
killstrombotz <killstrombotz@gmail.com>
2026-04-14 15:49:12 +0500

    Add files via upload

---
+++ devkillstrom/context.js
@@ -0,0 +1,39 @@
+module.exports = {
+  
+  getContextInfo: (ms) => {
+    return {
+      mentionedJid: [ms.sender || ms.from], 
+      forwardingScore: 999,
+      isForwarded: true, 
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363407848040840@newsletter', 
+        newsletterName: '𝗞𝗜𝗟𝗟𝗦𝗧𝗥𝗢𝗠-𝗠𝗗', 
+        serverMessageId: 143 
+      }
+    };
+  },
+
+  repondre: async (zk, dest, ms, text, options = {}) => {
+    const contextInfo = {
+      ...module.exports.getContextInfo(ms), 
+      ...options.contextInfo 
+    };
+
+    await zk.sendMessage(dest, {
+      text: text,
+      contextInfo: contextInfo
+    }); 
+  },
+
+  sendMessage: async (zk, dest, ms, options) => {
+    const contextInfo = {
+      ...module.exports.getContextInfo(ms), 
+      ...options.contextInfo 
+    };
+
+    await zk.sendMessage(dest, {
+      ...options,
+      contextInfo: contextInfo
+    }); 
+  }
+};
